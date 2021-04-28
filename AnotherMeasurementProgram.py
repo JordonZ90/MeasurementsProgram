ee6d82b783c8ff1e4905cb605eedc9f8d696df41
Vik <jzenterprize90@gmail.com>
2021-04-28 02:51:49 -0400

    seperated measurement programs to reduce menu clutter

---
+++ AnotherMeasurementProgram.py
@@ -0,0 +1,40 @@
+import moremeasurements
+
+
+def display_menu():
+    print("MENU")
+    print("1. Kilometers to Meters")
+    print("2. Meters to Kilometers")
+    print()
+
+def convert_measurements():
+    option = int(input("Enter a menu option "))
+    if option == 1:
+        kilometers = float(input("Enter measurement in kilometers "))
+        meters = moremeasurements.kilometers_to_meters(kilometers)
+        meters = round(meters, 2)
+        print(f"Measurement in kilometers {kilometers} converted to {meters} meters")
+    elif option == 2:
+        meters = float(input("Enter measurement in meters "))
+        kilometers = moremeasurements.meters_to_kilometers()
+        kilometers = round(kilometers, 2)
+        print(f"Measurement in meters {meters} converted to {kilometers} kilometers")
+    else:
+        print("Tou must enter a valid menu number")
+
+def main():
+    display_menu()
+    again = "y"
+    while again.lower() == "y":
+        convert_measurements()
+        print()
+        again = input("Convert another measurement? (y|n) ")
+        if again == "y".lower():
+            display_menu()
+        else:
+            print()
+    print("Goodbye!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ moremeasurements.py
@@ -0,0 +1,8 @@
+def kilometers_to_meters(kilometers):
+    meters = kilometers * 1000
+    return meters
+
+
+def meters_to_kilometers(meters):
+    kilometers = meters / 1000
+    return kilometers
